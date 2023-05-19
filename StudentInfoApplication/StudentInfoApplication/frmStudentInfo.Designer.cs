e521cc731ffd1a1b4b055050fa7330119a3b8a18
Peter John Arao <pitzzahh@gmail.com>
2023-05-19 09:52:34 +0900

    Add files via upload

---
+++ StudentInfoApplication/StudentInfoApplication/frmStudentInfo.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace StudentInfoApplication
+{
+    partial class frmStudentInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StudentIDInput = new System.Windows.Forms.TextBox();
+            this.LastNameInput = new System.Windows.Forms.TextBox();
+            this.FirstNameInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.StudentIDList = new System.Windows.Forms.ListBox();
+            this.LastNameList = new System.Windows.Forms.ListBox();
+            this.FirstNameList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID *";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(245, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LastName *";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(454, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FirstName *";
+            // 
+            // StudentIDInput
+            // 
+            this.StudentIDInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentIDInput.Location = new System.Drawing.Point(16, 34);
+            this.StudentIDInput.Name = "StudentIDInput";
+            this.StudentIDInput.Size = new System.Drawing.Size(173, 26);
+            this.StudentIDInput.TabIndex = 3;
+            // 
+            // LastNameInput
+            // 
+            this.LastNameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastNameInput.Location = new System.Drawing.Point(248, 34);
+            this.LastNameInput.Name = "LastNameInput";
+            this.LastNameInput.Size = new System.Drawing.Size(173, 26);
+            this.LastNameInput.TabIndex = 4;
+            // 
+            // FirstNameInput
+            // 
+            this.FirstNameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstNameInput.Location = new System.Drawing.Point(457, 34);
+            this.FirstNameInput.Name = "FirstNameInput";
+            this.FirstNameInput.Size = new System.Drawing.Size(173, 26);
+            this.FirstNameInput.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Student ID List";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(245, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "LastName List";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(454, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 18);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "FirstName List";
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitBtn.Location = new System.Drawing.Point(549, 67);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(80, 29);
+            this.SubmitBtn.TabIndex = 12;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // StudentIDList
+            // 
+            this.StudentIDList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentIDList.FormattingEnabled = true;
+            this.StudentIDList.ItemHeight = 16;
+            this.StudentIDList.Location = new System.Drawing.Point(16, 120);
+            this.StudentIDList.Name = "StudentIDList";
+            this.StudentIDList.Size = new System.Drawing.Size(173, 148);
+            this.StudentIDList.TabIndex = 13;
+            // 
+            // LastNameList
+            // 
+            this.LastNameList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastNameList.FormattingEnabled = true;
+            this.LastNameList.ItemHeight = 16;
+            this.LastNameList.Location = new System.Drawing.Point(248, 120);
+            this.LastNameList.Name = "LastNameList";
+            this.LastNameList.Size = new System.Drawing.Size(173, 148);
+            this.LastNameList.TabIndex = 14;
+            // 
+            // FirstNameList
+            // 
+            this.FirstNameList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstNameList.FormattingEnabled = true;
+            this.FirstNameList.ItemHeight = 16;
+            this.FirstNameList.Location = new System.Drawing.Point(457, 122);
+            this.FirstNameList.Name = "FirstNameList";
+            this.FirstNameList.Size = new System.Drawing.Size(173, 148);
+            this.FirstNameList.TabIndex = 15;
+            // 
+            // frmStudentInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 294);
+            this.Controls.Add(this.FirstNameList);
+            this.Controls.Add(this.LastNameList);
+            this.Controls.Add(this.StudentIDList);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.FirstNameInput);
+            this.Controls.Add(this.LastNameInput);
+            this.Controls.Add(this.StudentIDInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmStudentInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Information Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox StudentIDInput;
+        private System.Windows.Forms.TextBox LastNameInput;
+        private System.Windows.Forms.TextBox FirstNameInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.ListBox StudentIDList;
+        private System.Windows.Forms.ListBox LastNameList;
+        private System.Windows.Forms.ListBox FirstNameList;
+    }
+}
+
---
+++ StudentInfoApplication/StudentInfoApplication/frmStudentInfo.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInfoApplication
+{
+    public partial class frmStudentInfo : Form
+    {
+
+        private ArrayList StudentIdListContainer = new ArrayList();
+        private ArrayList LastNameListContainer = new ArrayList();
+        private ArrayList FirstNameListContainer = new ArrayList();
+
+        public frmStudentInfo()
+        {
+            InitializeComponent();
+            Student student = new Student();
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            var _studentId = StudentIDInput.Text;
+            var _lastName = LastNameInput.Text;
+            var _firstName = FirstNameInput.Text;
+
+            if (_studentId.Length == 0) // guard clause pattern
+            {
+                MessageBox.Show("Student ID is required!");
+                return;
+            }
+            if (_lastName.Length == 0) // guard clause pattern
+            {
+                MessageBox.Show("Last Name is required!");
+                return;
+            }
+            if (_firstName.Length == 0)
+            {
+                MessageBox.Show("First Name is required!");
+                return;
+            }
+
+            StudentIdListContainer.Add(_studentId);
+            LastNameListContainer.Add(_lastName);
+            FirstNameListContainer.Add(_firstName);
+
+            StudentIDList.Items.Clear();
+            LastNameList.Items.Clear();
+            FirstNameList.Items.Clear();
+
+            InsertData(StudentIDList, StudentIdListContainer);
+            InsertData(LastNameList, LastNameListContainer);
+            InsertData(FirstNameList, FirstNameListContainer);
+
+        }
+
+        private void InsertData(ListBox view, ArrayList data)
+        {
+            foreach (string item in data)
+            {
+                if (item != null)
+                {
+                    view.Items.Add(item);
+                }
+            }
+        }
+    }
+
+    class Student
+    {
+        private string StudentId;
+        private string LastName;
+        private string FirstName;
+
+        public Student() { }
+
+        public Student(string StudentId, string LastName, string FirstName)
+        {
+            this.StudentId = StudentId;
+            this.LastName = LastName;
+            this.FirstName = FirstName;
+        }
+
+        public string GetStudentId()
+        {
+            return StudentId;
+        }
+
+        public string GetLastName()
+        {
+            return LastName;
+        }
+
+        public string GetFirstName()
+        {
+            return FirstName;
+        }
+
+        public void SetStudentId(string StudentId)
+        {
+            this.StudentId = StudentId.Trim();
+        }
+
+        public void SetLastName(string LastName)
+        {
+            this.LastName = LastName.Trim();
+        }
+
+        public void SetFirstName(string FirstName)
+        {
+            this.FirstName = FirstName;
+        }
+
+    }
+}
