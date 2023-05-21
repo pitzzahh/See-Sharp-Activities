5b59c29a820f9564718f9913efb1ae1bc33ab897
Peter John Arao <pitzzahh@gmail.com>
2023-05-21 14:29:44 +0800

    Add files via upload

---
+++ EmployeeApplication/EmployeeApplication/frmEmployeeDatabase.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace EmployeeApplication
+{
+    partial class frmEmployeeDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EmployeeList = new System.Windows.Forms.DataGridView();
+            this.EmployeeID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EmployeeFirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EmployeeLastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Position = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EmployeeIDInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.EmployeeFirstNameInput = new System.Windows.Forms.TextBox();
+            this.EmployeeLastNameInput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.EmployeePositionInput = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeeList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Employee:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(224, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee List:";
+            // 
+            // EmployeeList
+            // 
+            this.EmployeeList.AllowUserToDeleteRows = false;
+            this.EmployeeList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmployeeList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EmployeeID,
+            this.EmployeeFirstName,
+            this.EmployeeLastName,
+            this.Position});
+            this.EmployeeList.Location = new System.Drawing.Point(228, 37);
+            this.EmployeeList.Name = "EmployeeList";
+            this.EmployeeList.Size = new System.Drawing.Size(485, 150);
+            this.EmployeeList.TabIndex = 2;
+            // 
+            // EmployeeID
+            // 
+            this.EmployeeID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.EmployeeID.HeaderText = "ID";
+            this.EmployeeID.Name = "EmployeeID";
+            this.EmployeeID.ReadOnly = true;
+            // 
+            // EmployeeFirstName
+            // 
+            this.EmployeeFirstName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.EmployeeFirstName.HeaderText = "First Name";
+            this.EmployeeFirstName.Name = "EmployeeFirstName";
+            this.EmployeeFirstName.ReadOnly = true;
+            // 
+            // EmployeeLastName
+            // 
+            this.EmployeeLastName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.EmployeeLastName.HeaderText = "Last Name";
+            this.EmployeeLastName.Name = "EmployeeLastName";
+            // 
+            // Position
+            // 
+            this.Position.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Position.HeaderText = "Position";
+            this.Position.Name = "Position";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Employee ID *";
+            // 
+            // EmployeeIDInput
+            // 
+            this.EmployeeIDInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeeIDInput.Location = new System.Drawing.Point(16, 68);
+            this.EmployeeIDInput.Name = "EmployeeIDInput";
+            this.EmployeeIDInput.Size = new System.Drawing.Size(206, 24);
+            this.EmployeeIDInput.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "First Name *";
+            // 
+            // EmployeeFirstNameInput
+            // 
+            this.EmployeeFirstNameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeeFirstNameInput.Location = new System.Drawing.Point(16, 126);
+            this.EmployeeFirstNameInput.Name = "EmployeeFirstNameInput";
+            this.EmployeeFirstNameInput.Size = new System.Drawing.Size(206, 24);
+            this.EmployeeFirstNameInput.TabIndex = 6;
+            // 
+            // EmployeeLastNameInput
+            // 
+            this.EmployeeLastNameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeeLastNameInput.Location = new System.Drawing.Point(16, 181);
+            this.EmployeeLastNameInput.Name = "EmployeeLastNameInput";
+            this.EmployeeLastNameInput.Size = new System.Drawing.Size(206, 24);
+            this.EmployeeLastNameInput.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Last Name *";
+            // 
+            // EmployeePositionInput
+            // 
+            this.EmployeePositionInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeePositionInput.Location = new System.Drawing.Point(16, 235);
+            this.EmployeePositionInput.Name = "EmployeePositionInput";
+            this.EmployeePositionInput.Size = new System.Drawing.Size(206, 24);
+            this.EmployeePositionInput.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 18);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Position *";
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.SubmitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SubmitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.SubmitBtn.Location = new System.Drawing.Point(53, 265);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(114, 34);
+            this.SubmitBtn.TabIndex = 11;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = false;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // frmEmployeeDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 306);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.EmployeePositionInput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.EmployeeLastNameInput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.EmployeeFirstNameInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EmployeeIDInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EmployeeList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEmployeeDatabase";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Database";
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeeList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView EmployeeList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmployeeID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmployeeFirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmployeeLastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Position;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox EmployeeIDInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox EmployeeFirstNameInput;
+        private System.Windows.Forms.TextBox EmployeeLastNameInput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox EmployeePositionInput;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button SubmitBtn;
+    }
+}
+
---
+++ EmployeeApplication/EmployeeApplication/frmEmployeeDatabase.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using EmployeeNamespace;
+using System.Windows.Forms;
+
+namespace EmployeeApplication
+{
+    public partial class frmEmployeeDatabase : Form
+    {
+        public frmEmployeeDatabase()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            var _id = EmployeeFirstNameInput.Text;
+            var _firstName = EmployeeFirstNameInput.Text;
+            var _lastName = EmployeeLastNameInput.Text;
+            var _position = EmployeePositionInput.Text;
+
+            if (_id.Length == 0) // guard clause pattern
+            {
+                MessageBox.Show("ID is required!");
+                return;
+            }
+            if (_firstName.Length == 0)
+            {
+                MessageBox.Show("First Name is required!");
+                return;
+            }
+            if (_lastName.Length == 0)
+            {
+                MessageBox.Show("Last Name is required!");
+                return;
+            }
+
+            if (_position.Length == 0)
+            {
+                MessageBox.Show("Position is required!");
+                return;
+            }
+
+            var employee = new Employee(_id, _firstName, _lastName, _position);
+
+            EmployeeList.Rows.Add(
+                employee.EmployeeID, 
+                employee.EmployeeFirstName, 
+                employee.EmployeeLastName, 
+                employee.Position
+            );
+
+            EmployeeIDInput.Text = string.Empty;
+            EmployeeFirstNameInput.Text = string.Empty;
+            EmployeeLastNameInput.Text = string.Empty;
+            EmployeePositionInput.Text = string.Empty;
+        }
+    }
+}
+namespace EmployeeNamespace
+{
+    public class Employee
+    {
+        private string employeeID;
+        private string employeeFirstName;
+        private string employeeLastName;
+        private string position;
+
+        public string EmployeeID
+        {
+            get { return employeeID; }
+            set { employeeID = value; }
+        }
+
+        public string EmployeeFirstName
+        {
+            get { return employeeFirstName; }
+            set { employeeFirstName = value; }
+        }
+
+        public string EmployeeLastName
+        {
+            get { return employeeLastName; }
+            set { employeeLastName = value; }
+        }
+
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Employee(string employeeID, string employeeFirstName, string employeeLastName, string position)
+        {
+            this.employeeID = employeeID;
+            this.employeeFirstName = employeeFirstName;
+            this.employeeLastName = employeeLastName;
+            this.position = position;
+        }
+
+        public Employee(string employeeID, string employeeFirstName, string employeeLastName)
+        {
+            this.employeeID = employeeID;
+            this.employeeFirstName = employeeFirstName;
+            this.employeeLastName = employeeLastName;
+        }
+
+        public Employee(string employeeID, string position)
+        {
+            this.employeeID = employeeID;
+            this.position = position;
+        }
+
+        public Employee()
+        {
+            this.employeeID = "55555555";
+            this.employeeFirstName = "Default First Name";
+            this.employeeLastName = "Default Last Name";
+            this.position = "Default Position";
+        }
+    }
+}
